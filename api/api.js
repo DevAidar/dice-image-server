6f687f5c973225d55d6617d50048081f1a52b543
Aidar <limepeach26@gmail.com>
2021-04-20 16:52:19 -0700

    [Add] Error handler middleware

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 
+const middlewares = require('../middlewares/middlewares');
 const { create, deleteImageById } = require('./controllers/image-controllers');
 const User = require('./models/user.js');
 
@@ -36,5 +37,8 @@ router.post('/', verifyToken, create);
 router.delete('/', verifyToken, deleteImageById, (_, res) => {
 	res.send('Successfully Deleted an Image');
 });
+
+router.use(middlewares.notFound);
+router.use(middlewares.errorHandler);
 
 module.exports = router;
