2acfa4113554b207704c617d2ef31c035a97b1d7
Aidar <limepeach26@gmail.com>
2021-04-20 16:23:53 -0700

    [Edit] Added throw statement to user check

--- api/api.js
+++ api/api.js
@@ -4,22 +4,23 @@ const jwt = require('jsonwebtoken');
 const { create, deleteImageById } = require('./controllers/image-controllers');
 const User = require('./models/user.js');
 
-const verifyToken = async (req, res, next) => {
+const verifyToken = (req, res, next) => {
 	const authHeader = req.headers['access-token'];
 	const token = authHeader && authHeader.split(' ')[1];
 
-	if (!token) return res.status(500).send({ message: 'Access Denied' });
+	if (!token) return res.status(401).send({ message: 'Access Denied' });
 
 	try {
 		const verify = jwt.verify(token, process.env.TOKEN_SECRET);
 		req.userId = verify._id;
     
 		// Check if the user exists
-		await User.exists({ '_id': verify._id });
+		if (!User.exists({ '_id': verify._id }))
+			throw 'User does not exist';
     
 		next();
 	} catch (err) {
-		return res.status(500).send({ message: 'Invalid Token.' });
+		return res.status(403).send({ message: 'Invalid Token.' });
 	}
 };
 
