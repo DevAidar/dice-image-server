ffc0d35393d292abd8f84e26ab898df14232598e
Aidar <limepeach26@gmail.com>
2021-04-20 16:45:00 -0700

    [TEST]

--- api/api.js
+++ api/api.js
@@ -6,20 +6,27 @@ const User = require('./models/user.js');
 
 const verifyToken = (req, res, next) => {
 	const authHeader = req.headers['access-token'];
+	console.log('1');
 	const token = authHeader && authHeader.split(' ')[1];
+	console.log('2');
 
 	if (!token) return res.status(401).send({ message: 'Access Denied' });
+	console.log('3');
 
 	try {
 		const verify = jwt.verify(token, process.env.TOKEN_SECRET);
+		console.log('4');
 		req.userId = verify._id;
+		console.log('5');
     
 		// Check if the user exists
 		if (!User.exists({ '_id': verify._id }))
 			throw 'User does not exist';
     
+		console.log('6');
 		next();
 	} catch (err) {
+		console.log('7');
 		return res.status(403).send({ message: 'Invalid Token.' });
 	}
 };
