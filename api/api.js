6e29c546eb302b6dad3f175c3f55989c933d0fae
Aidar <limepeach26@gmail.com>
2021-04-21 15:14:36 -0700

    [TEST]

--- api/api.js
+++ api/api.js
@@ -5,7 +5,7 @@ const middlewares = require('../middlewares/middlewares');
 const { create, deleteImageById } = require('./controllers/image-controllers');
 const User = require('./models/user.js');
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
 	const authHeader = req.headers['access-token'];
 	console.log('1');
 	const token = authHeader && authHeader.split(' ')[1];
@@ -18,10 +18,10 @@ const verifyToken = (req, res, next) => {
 
 	jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
 		console.log('4');
-		console.log('err', err ? err : 'No Errors');
+		console.log('err', err ? 'There is an Error' : 'No Errors');
 		console.log('decoded', decoded ? decoded : 'Nothing to Decode');
 
-		if (err) {
+		if (!decoded) {
 			return res.status(403).send('Invalid Token.');
 		}
 
