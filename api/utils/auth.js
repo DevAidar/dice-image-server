de2c293cc5b6763a0391de7d071ea985a0ccb582
Aidar <limepeach26@gmail.com>
2021-04-21 15:55:42 -0700

    [TEST]

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -11,12 +11,12 @@ const verifyToken = (req, res, next) => {
 		const verify = jwt.verify(token, process.env.TOKEN_SECRET);
 		console.log(verify);
 		req.userId = verify._id;
-		// next();
+		next();
 	} catch (err) {
-		res.status(403).send('Invalid Token.');
+		// res.status(403).send('Invalid Token.');
+		next();
 	}
 
-	next();
 };
 
 module.exports = { verifyToken };
